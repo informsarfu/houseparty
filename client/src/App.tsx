fb25a9d180f728807b756d61f375b52fdb5b8484
Sarfraz <informsarfu@gmail.com>
2025-10-17 13:24:37 -0400

    Added react client and setup API connection from react to django using django-cors-header

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [status, setStatus] = useState("Loading...");
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/health')
+      .then(r => r.json())
+      .then(d => setStatus(d.message))
+      .catch(() => setStatus("Error connecting to API"));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>HouseParty Client!</h1>
+      <p>API Status: {status}</p>
+    </div>
+  );
+}
+
+export default App;
--- server/server/settings.py
+++ server/server/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core'
+    'core',
+    'corsheaders',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'server.urls'
 
@@ -122,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
