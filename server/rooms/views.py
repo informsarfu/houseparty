cdc8127c729316e2bf1c9d89a11f72fd09ab82a9
Sarfraz <informsarfu@gmail.com>
2025-10-17 22:55:02 -0400

    removed unwanted code from views.py

--- server/rooms/views.py
+++ server/rooms/views.py
@@ -1,5 +1,4 @@
 from rest_framework.decorators import api_view, permission_classes
-
 from .models import Room, RoomFiles
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import RoomSerializer, RoomFilesSerializer, UserSerializer
@@ -21,22 +20,6 @@ class RoomListView(APIView):
         new_room.users.add(request.user)
         serializer = RoomSerializer(new_room)
         return Response(serializer.data)
-
-# @api_view(['GET','POST'])
-# @permission_classes([IsAuthenticated])
-# def rooms(request):
-#     if request.method == 'GET':
-#         all_rooms = Room.objects.filter(users=request.user) | Room.objects.filter(host=request.user)
-#         serializer = RoomSerializer(all_rooms, many=True)
-#         return Response(serializer.data)
-
-#     elif request.method == "POST":
-#         room_name = request.data.get('name')
-#         new_room = Room.objects.create(name=room_name, host=request.user)
-#         new_room.users.add(request.user)
-#         serializer = RoomSerializer(new_room)
-#         return Response(serializer.data)
-
 
 
 # Delete room, get room details
@@ -60,27 +43,6 @@ class RoomView(APIView):
             return Response({"error": "Only the host can delete the room."}, status=403)
         room.delete()
         return Response({"message": "Room deleted successfully."}, status=200)
-
-
-# @api_view(['GET', 'DELETE'])
-# @permission_classes([IsAuthenticated])
-# def room(request, room_code):
-#     if request.method == 'GET':
-#         if not Room.objects.filter(room_code=room_code).exists():
-#             return Response({"error": "Room not found."}, status=404)
-        
-#         room_info = Room.objects.get(room_code=room_code)
-#         serializer = RoomSerializer(room_info)
-#         return Response(serializer.data)
-
-#     elif request.method == 'DELETE':
-#         room = Room.objects.get(room_code = room_code)
-#         if not room:
-#             return Response({"error": "Room not found."}, status=404)
-#         if room.host != request.user:
-#             return Response({"error": "Only the host can delete the room."}, status=403)
-#         room.delete()
-#         return Response({"message": "Room deleted successfully."}, status=200)
     
 
 # Join room, Leave room
