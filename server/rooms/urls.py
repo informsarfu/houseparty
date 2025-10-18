36413e6198384a518d4ded8d4b6c41ac1ae8e3db
Sarfraz <informsarfu@gmail.com>
2025-10-17 22:54:14 -0400

    converted Function based API to class based API

--- server/rooms/urls.py
+++ server/rooms/urls.py
@@ -1,22 +1,13 @@
 from django.urls import path
+from .views import RoomListView, RoomView
 from . import views
 
 urlpatterns = [
-    path('', views.rooms, name='rooms'),
-    path('<str:room_code>/', views.room, name='room'),
+    path('', RoomListView.as_view(), name='all_rooms'),
+    path('<str:room_code>/', RoomView.as_view(), name='room'),
     path('<str:room_code>/access/', views.room_access, name='room_access'),
     path('<str:room_code>/files/', views.files, name='files'),
 ]
 
-
-#view all rooms
-#create a room
-#delete a room
-#join a room
-#leave a room
-
-
-#get room details
-#upload file to room
 
 
--- server/rooms/views.py
+++ server/rooms/views.py
@@ -1,40 +1,58 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
+
+from .models import Room, RoomFiles
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import RoomSerializer, RoomFilesSerializer, UserSerializer
-from .models import Room, RoomFiles
-from django.contrib.auth.models import User
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # View all rooms of user, create room
-@api_view(['GET','POST'])
-@permission_classes([IsAuthenticated])
-def rooms(request):
-    if request.method == 'GET':
+class RoomListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
         all_rooms = Room.objects.filter(users=request.user) | Room.objects.filter(host=request.user)
         serializer = RoomSerializer(all_rooms, many=True)
         return Response(serializer.data)
 
-    elif request.method == "POST":
+    def post(self, request):
         room_name = request.data.get('name')
         new_room = Room.objects.create(name=room_name, host=request.user)
         new_room.users.add(request.user)
         serializer = RoomSerializer(new_room)
         return Response(serializer.data)
 
+# @api_view(['GET','POST'])
+# @permission_classes([IsAuthenticated])
+# def rooms(request):
+#     if request.method == 'GET':
+#         all_rooms = Room.objects.filter(users=request.user) | Room.objects.filter(host=request.user)
+#         serializer = RoomSerializer(all_rooms, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == "POST":
+#         room_name = request.data.get('name')
+#         new_room = Room.objects.create(name=room_name, host=request.user)
+#         new_room.users.add(request.user)
+#         serializer = RoomSerializer(new_room)
+#         return Response(serializer.data)
+
+
+
 # Delete room, get room details
-@api_view(['GET', 'DELETE'])
-@permission_classes([IsAuthenticated])
-def room(request, room_code):
-    if request.method == 'GET':
+
+class RoomView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, room_code):
         if not Room.objects.filter(room_code=room_code).exists():
             return Response({"error": "Room not found."}, status=404)
         
         room_info = Room.objects.get(room_code=room_code)
         serializer = RoomSerializer(room_info)
         return Response(serializer.data)
-
-    elif request.method == 'DELETE':
+    
+    def delete(self, request, room_code):
         room = Room.objects.get(room_code = room_code)
         if not room:
             return Response({"error": "Room not found."}, status=404)
@@ -42,6 +60,27 @@ def room(request, room_code):
             return Response({"error": "Only the host can delete the room."}, status=403)
         room.delete()
         return Response({"message": "Room deleted successfully."}, status=200)
+
+
+# @api_view(['GET', 'DELETE'])
+# @permission_classes([IsAuthenticated])
+# def room(request, room_code):
+#     if request.method == 'GET':
+#         if not Room.objects.filter(room_code=room_code).exists():
+#             return Response({"error": "Room not found."}, status=404)
+        
+#         room_info = Room.objects.get(room_code=room_code)
+#         serializer = RoomSerializer(room_info)
+#         return Response(serializer.data)
+
+#     elif request.method == 'DELETE':
+#         room = Room.objects.get(room_code = room_code)
+#         if not room:
+#             return Response({"error": "Room not found."}, status=404)
+#         if room.host != request.user:
+#             return Response({"error": "Only the host can delete the room."}, status=403)
+#         room.delete()
+#         return Response({"message": "Room deleted successfully."}, status=200)
     
 
 # Join room, Leave room
