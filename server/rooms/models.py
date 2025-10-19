dfea14479f09eaa3d77be0d4840a5fc0fbbc4038
Sarfraz <informsarfu@gmail.com>
2025-10-19 18:43:58 -0400

    added custom delete functions to delete files from mediapath upon room delete and individual file delete

--- server/rooms/models.py
+++ server/rooms/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 import string
 import random
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+
 
 def generate_room_code():
     size = 6
@@ -17,6 +21,11 @@ class Room(models.Model):
     room_code = models.CharField(max_length=8, unique=True, default=generate_room_code)
     users = models.ManyToManyField(User, related_name='rooms', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        for file_obj in self.files.all():
+            file_obj.delete()
+        super().delete(*args, **kwargs)
     
 
 class RoomFiles(models.Model):
@@ -24,3 +33,7 @@ class RoomFiles(models.Model):
     file = models.FileField(upload_to='room_files/')
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        self.file.delete()
+        super().delete(*args, **kwargs)
