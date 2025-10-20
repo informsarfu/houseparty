554c3a4435779e675c300451ba6e8b5e4f62fed9
Sarfraz <informsarfu@gmail.com>
2025-10-19 23:16:16 -0400

    added files to protected routes - implementation pending

--- client/src/ProtectedRoutes.tsx
+++ client/src/ProtectedRoutes.tsx
@@ -1,23 +1,23 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+// import { Navigate, Outlet } from 'react-router-dom'
+// import { useState, useEffect } from 'react'
+// import axios from 'axios'
 
-const ProtectedRoutes = () => {
-  const token = localStorage.getItem('token');
-  const [isValid, setIsValid] = useState(true);
-  const verifyUrl = "http://127.0.0.1:8000/" + "api/auth/token/verify/";
+// const ProtectedRoutes = () => {
+//   const token = localStorage.getItem('token');
+//   const [isValid, setIsValid] = useState(true);
+//   const verifyUrl = "http://127.0.0.1:8000/" + "api/auth/token/verify/";
 
-  useEffect(() => {
-    if(!token){
-      setIsValid(false);
-      return;
-    }
-    setIsValid(true);
-  }, [token])
+//   useEffect(() => {
+//     if(!token){
+//       setIsValid(false);
+//       return;
+//     }
+//     setIsValid(true);
+//   }, [token])
 
-return (
-    (isValid) ? <Outlet/> : <Navigate to='/'/>
-  )
-}
+// return (
+//     (isValid) ? <Outlet/> : <Navigate to='/'/>
+//   )
+// }
 
-export default ProtectedRoutes
+// export default ProtectedRoutes
